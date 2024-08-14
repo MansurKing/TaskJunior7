0e140d31bb1ac56ec810f84ce287f7e2ce6f4701
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-14 14:45:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaskJunior7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInLine;
+            int waitingTimeMinutes = 10;
+            int minutesOfWaiting = 60;
+            int expectationInHours;
+            int expectationInMinutes;
+
+            Console.WriteLine("Приветствую тебя дорогой посититель нашей полеклиники!\nДля облегчения пользованием нашей полеклиники была созданна программа, которая обределяет кол-во времение ожидания в живой очедеди\n");
+            Console.Write("Введите кол-во людей: ");
+            peopleInLine = Convert.ToInt32(Console.ReadLine());
+
+            expectationInHours = (peopleInLine * waitingTimeMinutes) / minutesOfWaiting;
+            expectationInMinutes = (peopleInLine * waitingTimeMinutes) % minutesOfWaiting;
+
+            Console.Write($"\nКол-во времени ожидания вашей очереди составляет: {expectationInHours} часов и {expectationInMinutes} минут ");
+        }
+    }
+}
