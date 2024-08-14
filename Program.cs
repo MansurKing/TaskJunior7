b54937299b56fa865e8d12d258eb18615d18b768
MansurKing <102825225+MansurKing@users.noreply.github.com>
2024-08-14 15:36:37 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace TaskJunior7
         static void Main(string[] args)
         {
             int peopleInLine;
-            int waitingTimeMinutes = 10;
+            int waitingLine = 10;
             int minutesOfWaiting = 60;
             int expectationInHours;
             int expectationInMinutes;
@@ -16,8 +16,8 @@ namespace TaskJunior7
             Console.Write("Введите кол-во людей: ");
             peopleInLine = Convert.ToInt32(Console.ReadLine());
 
-            expectationInHours = (peopleInLine * waitingTimeMinutes) / minutesOfWaiting;
-            expectationInMinutes = (peopleInLine * waitingTimeMinutes) % minutesOfWaiting;
+            expectationInHours = (peopleInLine * waitingLine) / minutesOfWaiting;
+            expectationInMinutes = (peopleInLine * waitingLine) % minutesOfWaiting;
 
             Console.Write($"\nКол-во времени ожидания вашей очереди составляет: {expectationInHours} часов и {expectationInMinutes} минут ");
         }
